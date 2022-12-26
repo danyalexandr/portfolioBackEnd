0f02e8887ffd1f25f8598bd2fcadd0af20de0f8f
danyalexandr <danyalexandr@gmail.com>
2022-12-25 21:08:29 -0300

    prueba cors home

--- src/main/java/com/argentina/programa/jwt/test/configuration/WebSecurityConfig.java
+++ src/main/java/com/argentina/programa/jwt/test/configuration/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable()
 				// Los endpoints /login y /register no necesitan ser autenticados.			
-				.authorizeRequests().antMatchers("/login","/register").permitAll().
+				.authorizeRequests().antMatchers("/login","/register", "/home").permitAll().
 				// El resto de los endpoints necesita el token JWT para validar el request.
 				anyRequest().authenticated().and().
 				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
